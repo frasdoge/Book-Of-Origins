99d338fa51a18e3dad7bc124bbe98e5c1f1c733a
frasdoge <frasdoge@gmail.com>
2020-06-28 23:53:30 +1000

    Can now change race talents at will.
    Added the ability to change the race-based talents. Requires the extender to fix the talents not showing up in the talent pane UI.
    
    Unfortunate but unavoidable, and most users either won't care or will already be using the extender.
    
    Translations yet to be implemented.

---
+++ Mods/BookOfOrigins_395a6fbb-f855-4cb0-97a9-6674bfd1799b/Story/RawFiles/Lua/FixTalents.lua
@@ -0,0 +1,58 @@
+local function GetArrayIndexStart(ui, arrayName, offset)
+    local i = 0
+    while i < 9999 do
+        local val = ui:GetValue(arrayName, "number", i)
+        if val == nil then
+            val = ui:GetValue(arrayName, "string", i)
+            if val == nil then
+                val = ui:GetValue(arrayName, "boolean", i)
+            end
+        end
+        if val == nil then
+            return i
+        end
+        i = i + offset
+    end
+    return -1
+end
+
+local raceTalents = {["Lizard_Persuasion"] = {94, "ha4af67a7g7112g4e66gbaedg6bf024feb097"},
+                    ["Elf_Lore"]           = {89, "hcfd646bdg491dg4d9cgaf1ag2ca5f4421f7b"}, 
+                    ["Dwarf_Sneaking"]     = {92, "h429e53b9ge574g4c77gbc1ag2cfd9844252f"}, 
+                    ["Human_Civil"]        = {88, "h6c44d6c0g4603g429ag9f5bgc4ba0460fdec"}, 
+                    ["Lizard_Resistance"]  = {93, "h7b9a0d2egff87g42afgbec4g6c01c4303401"}, 
+                    ["Elf_CorpseEating"]   = {90, "h8fcf368eg0abeg4314gacdcgb495473a9ade"}, 
+                    ["Dwarf_Sturdy"]       = {91, "h477b8976gfac3g4cdag954bg5617876c6ef7"}, 
+                    ["Human_Inventive"]    = {87, "h2646745cgf1b5g44a2gaf6ageef8ee73a923"}}
+
+local function FixRaceTalents(ui, call, ...)
+    local charHandle = ui:GetPlayerHandle()
+    local stats = Ext.GetCharacter(charHandle).Stats
+
+    local j = GetArrayIndexStart(ui, "talent_array", 1)
+    local k = 0
+
+    for talent, info in pairs(raceTalents) do
+
+        if stats["TALENT_" .. talent] then
+            
+            local str = Ext.GetTranslatedString(info[2], talent)
+            Ext.Print(str)
+
+            ui:SetValue("talent_array", str, j + 3*k)
+            ui:SetValue("talent_array", info[1], j + 1 + 3*k)
+            ui:SetValue("talent_array", 0, j + 2 + 3*k)
+
+            k = k + 1
+        end
+
+    end
+    
+end
+
+local function SessionLoaded()
+    local ui = Ext.GetBuiltinUI("Public/Game/GUI/characterSheet.swf")
+    Ext.RegisterUIInvokeListener(ui, "updateArraySystem", FixRaceTalents)
+end
+
+Ext.RegisterListener("SessionLoaded", SessionLoaded)
